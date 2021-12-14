7ccdc05e29542e2e3fc635397edd18c7f55444a9
Mark Gray <mark-github@vatavia.net>
2021-12-14 18:10:08 -0500

    Advent of Code day 7: The Treachery of Whales
    What end position can be reached by all with the minimum total use of fuel

---
+++ 07.1.rb
@@ -0,0 +1,15 @@
+# Advent of Code day 7: The Treachery of Whales
+# https://adventofcode.com/2021/day/7
+require 'csv'
+filename = '07-input.txt'
+start_positions = CSV.read(filename, converters: :numeric)[0]
+
+def total_cost(start_positions, end_position)
+  start_positions.inject(0) { |total, start| total + (start - end_position).abs }
+end
+
+min_position, max_position = start_positions.minmax
+costs = (min_position..max_position).map { |end_position| total_cost(start_positions, end_position) }
+puts "positions : #{min_position}..#{max_position}"
+puts "costs : #{costs}"
+puts "Value and position of minimum cost : #{costs.each_with_index.min}"
